10daa3d53ba8df8d6f913bc44730ac6663bb7b87
Nur Muhammad Luthfi <luthfi2444@gmail.com>
2019-08-19 07:46:59 +0700

    add language preference settings

--- app/src/main/java/com/android/consumermoviecatalogue/ui/MainActivity.java
+++ app/src/main/java/com/android/consumermoviecatalogue/ui/MainActivity.java
@@ -1,11 +1,15 @@
 package com.android.consumermoviecatalogue.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.android.consumermoviecatalogue.R;
 import com.android.consumermoviecatalogue.adapter.MainActivityAdapter;
@@ -22,6 +26,21 @@ public class MainActivity extends AppCompatActivity {
 
         initView();
         initActionBar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            Intent mIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(mIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initActionBar() {
