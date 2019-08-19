9cae9808eeb0d9d641045064f14c5a1a5110e3fa
Nur Muhammad Luthfi <luthfi2444@gmail.com>
2019-08-19 08:06:36 +0700

    add view pager, tab layout, change language

--- app/src/main/java/com/android/consumermoviecatalogue/MainActivity.java
+++ app/src/main/java/com/android/consumermoviecatalogue/MainActivity.java
@@ -1,7 +1,15 @@
 package com.android.consumermoviecatalogue;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initActionBar();
+    }
+
+    private void initActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setElevation(0f);
+        }
+    }
+
+    private void initView() {
+        ViewPager viewPager = findViewById(R.id.viewpager);
+
+
+        MainActivityFragmentAdapter adapter = new MainActivityFragmentAdapter(this,
+                getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.tv_show);
+        tabLayout.getTabAt(1).setIcon(R.drawable.video_camera);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+       setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            Intent mIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(mIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
